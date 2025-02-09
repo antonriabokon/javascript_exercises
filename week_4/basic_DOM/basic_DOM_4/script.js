6bd0f2bc10224682f7a2c3f5cfbaa2c79aa2fefb
antonriabokon <s2500139@edu.bc.fi>
2025-02-09 21:51:20 +0200

    dom_4

---
+++ week_4/basic_DOM/basic_DOM_4/script.js
@@ -0,0 +1,9 @@
+/*
+Create an HTML file with a <p> element and a button.
+Write JavaScript to toggle the visibility of the paragraph each time the button is clicked.
+ */
+
+function hideText() {
+    const textTog = document.getElementById("text");
+    text.style.display = text.style.display === "none" ? "block" : "none";
+}
