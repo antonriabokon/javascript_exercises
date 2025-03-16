9bb9bb452ac4c7a17230061038c9b6e3c774529d
antonriabokon <s2500139@edu.bc.fi>
2025-03-16 17:42:46 +0200

    added comments

--- week_4/atm_simulator/js/script.js
+++ week_4/atm_simulator/js/script.js
@@ -1,15 +1,18 @@
-const pin = 1111;
+const pin = 1111; // ATM pin code
 
+// Pincode check function
 function login() {
     const enteredPin = Number(document.getElementById("pin").value.trim());
     const logMessage = document.getElementById("loginMessage");
+    
+    // Test console logs
     console.log(enteredPin);
     console.log(pin);
-  
+    
     if (enteredPin === pin) {
-      document.getElementById("loginContainer").style.display = "none";
-      document.getElementById("atmCont").style.display = "block";
-      showMessage("Login successful!");
+      document.getElementById("loginContainer").style.display = "none"; // Hides pincode field
+      document.getElementById("atmCont").style.display = "block"; // Shows ATM display
+      showMessage("Login successful!"); // showMessage function, defined down below
     } else {
       logMessage.textContent = "Incorrect PIN. Please try again.";
       logMessage.style.color = "blue";
@@ -17,41 +20,49 @@ function login() {
   }
 document.getElementById("logBtn").addEventListener("click", login);
 
-let balance = 0;
+let balance = 0; // Start balance
 
+// Function to display messages and error warnings
+function showMessage(message) {
+
+  document.getElementById("message").textContent = message;
+}
+
+// Function to update and display balance after deposit or withdraw(different from message function)
 function displayBalance() {
   document.getElementById("balance").textContent = balance.toFixed(2);
 }
 
+// Deposit function
 function deposit() {
   const depositAmount = parseFloat(document.getElementById("depositAmount").value);
 
- 
-  if (isNaN(depositAmount) || depositAmount <= 0) {
+  // Checking the input 
+  if (isNaN(depositAmount) || depositAmount <= 0) {   
     showMessage("Please enter a valid amount.");
     document.getElementById("depositAmount").value = "";
     return;
   }
-
-
+  // Calculating current balance
   balance += depositAmount;
   displayBalance(); 
 
   showMessage(`Deposited ${depositAmount.toFixed(2)}!`);
   document.getElementById("depositAmount").value = "";
 }
+document.getElementById("depBtn").addEventListener("click", deposit);
 
-
+// Withdraw function
 function withdraw() {
 
   const withdrawAmount = parseFloat(document.getElementById("withdrawAmount").value);
 
+  // Checking the input
   if (isNaN(withdrawAmount) || withdrawAmount <= 0) {
     showMessage("Please enter a valid amount.");
     document.getElementById("withdrawAmount").value = "";
     return;
   }
-
 
   if (withdrawAmount > balance) {
     showMessage("Not enough balance!");
@@ -59,24 +70,13 @@ function withdraw() {
     return;
   }
 
-
+  // Calculating curent balance
   balance -= withdrawAmount;
   displayBalance(); 
 
-
   showMessage(`Withdrew ${withdrawAmount.toFixed(2)}!`);
-  document.getElementById("withdrawAmount").value = "";
+  document.getElementById("withdrawAmount").value = ""; 
 }
-
-
-function showMessage(message) {
-
-  document.getElementById("message").textContent = message;
-}
-
-
-document.getElementById("depBtn").addEventListener("click", deposit);
 document.getElementById("withBtn").addEventListener("click", withdraw);
-
 
 displayBalance();
--- week_4/pancake/js/script.js
+++ week_4/pancake/js/script.js
@@ -4,41 +4,51 @@ const extras = document.querySelectorAll('.extra');
 const totalPriceDisplay = document.getElementById('totalPriceDisplay');
 const totalPrice = document.getElementById('totalPrice'); 
 
+// Adding listener to the pancake selector
 pancakeType.addEventListener('change', updateTotalPrice);
 
+// Adding listener to the toppings
 toppings.forEach(topping => {
   topping.addEventListener('change', updateTotalPrice);
 });
 
+// Adding listner to the extras
 extras.forEach(extra => {
   extra.addEventListener('change', updateTotalPrice);
 });
 
+// Defining the function to calculate and update any changes
 function updateTotalPrice() {
   const selectedPancakeType = pancakeType.value;
-  let totalPriceValue = parseFloat(selectedPancakeType) || 0; 
+  let totalPriceValue = parseFloat(selectedPancakeType); 
   console.log(totalPriceValue);
 
+  // Adding toppings if checked 
   toppings.forEach(topping => {
     if (topping.checked) {
       totalPriceValue += parseFloat(topping.value);
+      console.log(totalPriceValue);
     }
   });
 
+  // Adding extras if checked
   extras.forEach(extra => {
     if (extra.checked) {
       totalPriceValue += parseFloat(extra.value);
+      console.log(totalPriceValue);
     }
   });
 
-
+  console.log(totalPriceValue);
+  
+  // Updating prices on the web page
   totalPrice.textContent = `${totalPriceValue}€`; 
   totalPriceDisplay.textContent = `${totalPriceValue}€`; 
 
-  const priceBanner = document.querySelector('.price-banner');
+  // Animation, price-banner on price change
+  const priceBanner = document.querySelector('.price-banner'); 
   priceBanner.classList.add('fade-in');
 
-  // Remove fade class after animation ends (to allow re-triggering)
   setTimeout(() => {
     priceBanner.classList.remove('fade-in');
   }, 500);
